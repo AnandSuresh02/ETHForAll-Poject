e68199dc6df7297ae6eb492dfec46224a14aa0b1
Anand S <anandzzz360@gmail.com>
2023-02-18 14:13:42 +0530

    ethforall: Initial upload

---
+++ Components/index.js
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar/NavBar";
+import Error from "./Error/Error";
+import Filter from "./Filter/Filter";
+import Loader from "./Loader/Loader";
+import Model from "./Model/Model";
+import UserCard from "./UserCard/UserCard";
+import Friend from "./Friend/Friend";
+
+export { NavBar, Error, Filter, Loader, Model, UserCard, Friend };
